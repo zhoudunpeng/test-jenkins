dca90c3552131339edd8c77b08a088fde7888f38
zhoudunpeng <zhoudunpeng@canpoint.com>
2020-11-26 15:12:54 +0800

    first commit

---
+++ src/main/java/com/zdp/springbootquickstart/Controller.java
@@ -0,0 +1,22 @@
+package com.zdp.springbootquickstart;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sesshomaru
+ * @date 2020/11/26
+ */
+@RestController
+public class Controller {
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello, my is " + port;
+    }
+}
+
